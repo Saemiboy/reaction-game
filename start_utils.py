2e34173936bb9e3eb29752c1a4bfcbc76a2f4d8c
Samuel Egli <samuelegligarcia@outlook.de>
2025-05-07 16:17:02 +0200

    startdatei wurde erstellt

---
+++ start_utils.py
@@ -0,0 +1,15 @@
+import pygame
+
+# --- Farben ---
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (50, 255, 50)
+RED = (255, 0, 0)
+BLUE = (0, 100, 255)
+
+class Home:
+    def __init__(self, width, height, font):
+        self.width = width
+        self.height = height
+        self.font = font
+        
