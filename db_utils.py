8585c0e7dbb7590cb5fe3d10d05db4cd4d76b3f7
Samuel Egli <samuelegligarcia@outlook.de>
2025-05-10 11:25:31 +0200

    Insert Methode gebaut für game einfügen in mysql table

--- db_utils.py
+++ db_utils.py
@@ -13,17 +13,31 @@ class DBClient:
             remote_bind_address=(db_host, db_port),
             local_bind_address=('127.0.0.1', local_bind_port)
         )
-        self.server.start()
+        try:
+            self.server.start()
+        except Exception as e:
+            print(f'Fehler bei der Verbindung: {e}')
 
-        self.connection = pymysql.connect(
-            host=db_host,
-            port=self.server.local_bind_port,
-            user=db_user,
-            password=db_pw,
-            database=db_name,
-        )
+        try:
+            self.connection = pymysql.connect(
+                host=db_host,
+                port=self.server.local_bind_port,
+                user=db_user,
+                password=db_pw,
+                database=db_name,
+            )
+        except pymysql.Error as er:
+            print(f'Fehler bei DB Verbindung: {er}')
+
         self.table = db_table
 
+    def insert_game(self, gamedata, spieler=None):
+        sql = f'INSERT INTO {self.table} ("SpielID", {"SpielerID" if spieler else "GaesteID"}, "Score", "Zeitbenoetigt", "Datum") VALUES (Null, %s, %s, %s, NOW())'
+        data = (gamedata[0], gamedata[1], gamedata[2])
+
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql, data)
+        self.connection.commit()
 
 
 
